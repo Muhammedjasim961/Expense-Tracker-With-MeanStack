9ce691a52c6e92dce46e946cfdf6e5200bd4f216
Muhammedjasim961 <jasimwayanad961@gmail.com>
2025-09-05 19:14:56 +0530

    first commit

--- Backend/Route/Auth.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const Router = express.Router();
-const AuthController = require("../Controllers/Auth.controller.js");
-Router.post("/register", AuthController.userRegister);
-Router.post("/login", AuthController.userLogin);
-
-module.exports = Router;
